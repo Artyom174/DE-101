f9bdff40d33fb47cf66abd6ea92e09de46eaecc4
Artyom Krasheninnikov <77455327+Artyom174@users.noreply.github.com>
2021-11-08 21:02:23 +0300

    Update script_for_postgres.py

--- module2/script_for_postgres.py
+++ module2/script_for_postgres.py
@@ -3,7 +3,7 @@ import psycopg2
 import psycopg2.extras
 import pandas as pd
 import pandas.io.sql as sqlio
-
+#при желании filename и section можно вынести и отдельно передавать в функцию.
 def config(filename='C:/Users/User/database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
@@ -20,35 +20,6 @@ def config(filename='C:/Users/User/database.ini', section='postgresql'):
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
 
     return db
-
-
-def check_connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-        # create a cursor
-        cur = conn.cursor()
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-        # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
 
 
 def read_tables(sql_query):
